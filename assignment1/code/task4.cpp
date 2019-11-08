58aca85857b1b34c1341d93e5b6c666e3ed6e29e
Trang Do Hoang Thu <s8trdoho@stud.uni-saarland.de>
2019-11-08 13:13:40 +0100

    task4.cpp

--- assignment1/code/task4.cpp
+++ assignment1/code/task4.cpp
@@ -1,3 +1,21 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <numeric>
+using namespace std;
+
 int main(int argc, char* argv[]) {
-  return 0;
+    string a; vector<string> va;
+    while(getline(cin,a)){
+        va.push_back(a);
+    }
+    for (int i=1; i<argc; ++i) {
+        string b = argv[i];
+        auto check_string = find(va.begin(), va.end(), b);
+        if (check_string == va.end()) cout << "not found" << endl;
+        else cout << check_string - va.begin() + 1 << endl;
+    }
+    return 0;
 }
