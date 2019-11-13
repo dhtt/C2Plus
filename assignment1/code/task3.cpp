f8d9f6283b4d219b6d3cfa78dbf263d741b74878
Trang Do Hoang Thu <s8trdoho@stud.uni-saarland.de>
2019-11-14 00:40:12 +0100

    Comment task3.cpp

--- assignment1/code/task3.cpp
+++ assignment1/code/task3.cpp
@@ -6,6 +6,7 @@
 #include <numeric>
 using namespace std;
 
+//functions prototypes
 void sort(std::vector<double>& v);
 double get_max(vector<double>& v);
 double get_min(vector<double>& v);
@@ -19,37 +20,37 @@ void sort(std::vector<double>& v) {
   std::sort(v.begin(), v.end());
 }
 
-double get_min(vector<double>& v) {
+double get_min(vector<double>& v) {//vector must be sorted first
     return(v[0]);
 }
-double get_max(vector<double>& v) {
+double get_max(vector<double>& v) {//vector must be sorted first
     return(v[v.size()-1]);
 }
 double get_mean(vector<double>& v) {
     return std::accumulate(v.begin(), v.end(), 0.0)/v.size();
 }
 
-double get_median(vector<double>& v){
-    if (v.size()%2 == 1) return v[v.size()/2];
-    else return (v[v.size()/2-1] + v[v.size()/2])/2;
+double get_median(vector<double>& v){//vector must be sorted first
+    if (v.size()%2 == 1) return v[v.size()/2]; //odd number of elements in vector: take middle value
+    else return (v[v.size()/2-1] + v[v.size()/2])/2; //even number of elements in vector: take average of 2 middle values
 }
-double get_first_quartile(vector<double>& v) {
-    vector<double> first_half(v.begin(), v.begin() + v.size() / 2);
-    return get_median(first_half);
-}
-
-double get_third_quartile(vector<double>& v){
-        if (v.size()%2 == 1) {
-            vector<double> second_half(v.begin() + v.size()/2 + 1, v.end());
-            return get_median(second_half);
-        }
-        else {
-            vector<double> second_half(v.begin() + v.size()/2, v.end());
-            return get_median(second_half);
-        }
+double get_first_quartile(vector<double>& v) {//vector must be sorted first
+    vector<double> first_half(v.begin(), v.begin() + v.size() / 2); //take first half of vector
+    return get_median(first_half); 
 }
 
-void get_result(vector<double> v, long type){
+double get_third_quartile(vector<double>& v){//vector must be sorted first
+        if (v.size()%2 == 1) { //odd number of elements in vector:
+            vector<double> second_half(v.begin() + v.size()/2 + 1, v.end()); //take second half of vector with middle value
+            return get_median(second_half);
+        }
+        else { //even number of elements in vector:
+            vector<double> second_half(v.begin() + v.size()/2, v.end()); //take second half of vector without middle value
+            return get_median(second_half);
+        }
+}
+
+void get_result(vector<double> v, long type){ //convenient results report
     sort(v);
 
     double quartile;
@@ -61,7 +62,7 @@ void get_result(vector<double> v, long type){
 }
 
 int main(int argc, char* argv[]) {
-    if (argc!=2){
+    if (argc!=2){ //give error when no quartile argument
         printf("Please give 1 argument.\n");
         return EXIT_FAILURE;
     }
@@ -69,7 +70,7 @@ int main(int argc, char* argv[]) {
     string line;
     vector<vector<double>> vvd; double d; double quartile;
 
-    while (getline(cin,line)) {
+    while (getline(cin,line)) { //for each line, store doubles in vector vd, then store vd in vector vvd
         std::istringstream iss(line);
         vector<double> vd;
         while(iss >> d){
@@ -77,10 +78,10 @@ int main(int argc, char* argv[]) {
         }
         vvd.push_back(vd);
     }
-    long type = strtol(argv[1], nullptr, 10);
+    
+    long type = strtol(argv[1], nullptr, 10); //read argument 1 for quartile
 
-
-    for (auto & vd : vvd) {
+    for (auto & vd : vvd) { //report result for each vector in vvd
         const vector<double>& row(vd);
         get_result(row, type);
     }
